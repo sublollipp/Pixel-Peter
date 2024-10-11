b5a1aa2d00dab846b5cd018c1e4e1829b4ba2a2e
Duirky <eriitenov@gmail.com>
2024-10-11 08:22:49 +0200

    Lowkey fikset preview
    Collisions fra på prview, altid 20 prikker

--- scener/jump_preview.gd
+++ scener/jump_preview.gd
@@ -1,6 +1,7 @@
 extends Line2D
 
 @onready var playercolmask = get_parent().collision_mask
+@export_custom(PROPERTY_HINT_LAYERS_2D_PHYSICS, "") var collision_mask = PROPERTY_HINT_LAYERS_2D_PHYSICS
 
 var grav = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -12,12 +13,13 @@ func make_points() -> void:
 	var yvel = get_parent().chargedJumpPower
 	var xvel = get_parent().chargedDirectionPower
 	clear_points()
-	for i in range(20):
+	for i in range(0, 20):
 		add_point(Vector2(i * dotGap * xvel, 0.5*grav*(i*dotGap)*(i*dotGap) - yvel * (i * dotGap)))
-		if (i < 2):
+		if (i < 1):
 			pass
 		else:
-			var query = PhysicsRayQueryParameters2D.create(get_point_position(i - 1), get_point_position(i), playercolmask)
+			var query = PhysicsRayQueryParameters2D.create(get_point_position(i - 1), get_point_position(i), collision_mask)
+			query.exclude = [self, get_parent()]
 			if space_state.intersect_ray(query):
 				add_point(space_state.intersect_ray(query).position)
 				print(space_state.intersect_ray(query))
@@ -26,6 +28,8 @@ func make_points() -> void:
 func _draw() -> void:
 	for i in range(get_point_count()):
 		draw_circle(get_point_position(i), 2, Color.WHITE)
+		if i == get_point_count() - 1:
+			draw_line(get_point_position(i - 1), get_point_position(i), Color.WHITE, 10)
 
 func _physics_process(delta: float) -> void:
 	space_state = get_world_2d().direct_space_state
